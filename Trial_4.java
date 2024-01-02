2fcf56e1090cd1553327c78c2d240855b247a6d9
Immanuel Fernandes <152952734+Immanuel-Fernandes@users.noreply.github.com>
2024-01-03 00:06:28 +0530

    Add files via upload

---
+++ Trial_4.java
@@ -0,0 +1,41 @@
+class Trial_4
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Byte Max Value: " + Byte.MAX_VALUE);
+        System.out.println("Byte Min Value: " + Byte.MIN_VALUE);
+
+        System.out.println("Short Max Value: " + Short.MAX_VALUE);
+        System.out.println("Short Min Value: " + Short.MIN_VALUE);
+
+        System.out.println("Int Max Value: " + Integer.MAX_VALUE);
+        System.out.println("Int Min Value: " + Integer.MIN_VALUE);
+
+        System.out.println("Long Max Value: " + Long.MAX_VALUE);
+        System.out.println("Long Min Value: " + Long.MIN_VALUE);
+
+        System.out.println("Float Max Value: " + Float.MAX_VALUE);
+        System.out.println("Float Min Value: " + Float.MIN_VALUE);
+
+        System.out.println("Double Max Value: " + Double.MAX_VALUE);
+        System.out.println("Double Min Value: " + Double.MIN_VALUE);
+    }
+}
+
+
+/* Output
+
+Byte Max Value: 127
+Byte Min Value: -128
+Short Max Value: 32767
+Short Min Value: -32768
+Int Max Value: 2147483647
+Int Min Value: -2147483648
+Long Max Value: 9223372036854775807
+Long Min Value: -9223372036854775808
+Float Max Value: 3.4028235E38
+Float Min Value: 1.4E-45
+Double Max Value: 1.7976931348623157E308
+Double Min Value: 4.9E-324
+
+*/
